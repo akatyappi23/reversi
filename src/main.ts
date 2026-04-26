0bb7f2f42949f1d5cbc9fcf668fd965e853e8645
akatyappi23 <aaaaaakagi23@gmail.com>
2026-04-26 17:15:03 +0900

    add game initialization logic and database integration for squares

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,109 @@
 import express from 'express'
+import 'express-async-errors'
 import morgan from 'morgan'
-import "express-async-errors"
 import mysql from 'mysql2/promise'
+
+const EMPTY = 0
+const DARK = 1
+const LIGHT = 2
+
+const INITIAL_BOARD = [
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, DARK, LIGHT, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, LIGHT, DARK, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY]
+]
 
 const PORT = 3000
 
 const app = express()
 
-app.use(morgan("dev"))
-app.use(express.static('static',{extensions:['html']}))
+app.use(morgan('dev'))
+app.use(express.static('static', { extensions: ['html'] }))
 
-app.get("/api/hello", async (req, res)=>{
-    res.json({
-        message: "hello express!!!"
-    })
+app.get('/api/hello', async (req, res) => {
+  res.json({
+    message: 'Hello Express!!!'
+  })
+})
+
+app.get('/api/error', async (req, res) => {
+  throw new Error('Error endpoint')
 })
 
 app.post('/api/games', async (req, res) => {
-    const startedAt = new Date()
-    
-    const conn = await mysql.createConnection({
-        host: 'localhost',
-        database: 'reversi',
-        user: 'reversi',
-        password: 'password'
+  const now = new Date()
+
+  const conn = await mysql.createConnection({
+    host: 'localhost',
+    database: 'reversi',
+    user: 'reversi',
+    password: 'password'
+  })
+  try {
+    await conn.beginTransaction()
+
+    const gameInsertResult = await conn.execute<mysql.ResultSetHeader>(
+      'insert into games (started_at) values (?)',
+      [now]
+    )
+    const gameId = gameInsertResult[0].insertId
+
+    const turnInsertResult = await conn.execute<mysql.ResultSetHeader>(
+      'insert into turns (game_id, turn_count, next_disc, end_at) values (?, ?, ?, ?)',
+      [gameId, 0, DARK, now]
+    )
+    const turnId = turnInsertResult[0].insertId
+
+    const squareCount = INITIAL_BOARD.map((line) => line.length).reduce(
+      (v1, v2) => v1 + v2,
+      0
+    )
+
+    const squaresInsertSql =
+      'insert into squares (turn_id, x, y, disc) values ' +
+      Array.from(Array(squareCount))
+        .map(() => '(?, ?, ?, ?)')
+        .join(', ')
+
+    const squaresInsertValues: any[] = []
+    INITIAL_BOARD.forEach((line, y) => {
+      line.forEach((disc, x) => {
+        squaresInsertValues.push(turnId)
+        squaresInsertValues.push(x)
+        squaresInsertValues.push(y)
+        squaresInsertValues.push(disc)
+      })
     })
 
-    try {
-        await conn.beginTransaction()
+    await conn.execute(squaresInsertSql, squaresInsertValues)
 
-        await conn.execute('insert into games (started_at) values (?)',[startedAt])
+    await conn.commit()
+  } finally {
+    await conn.end()
+  }
 
-        await conn.commit()
-    }finally{
-        await conn.end()
-    }
-
-    res.status(201).end()
+  res.status(201).end()
 })
 
 app.use(errorHandler)
 
-app.listen(PORT, ()=>{
-    console.log(`Reversi application started: http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Reversi application started: http://localhost:${PORT}`)
 })
 
-function errorHandler(err: any, _req: express.Request, res: express.Response, _next: express.NextFunction){
-    console.error("Unexpected error occurred", err)
-    res.status(500).send({
-        message: "Unexpected error occurred"
-    })
+function errorHandler(
+  err: any,
+  _req: express.Request,
+  res: express.Response,
+  _next: express.NextFunction
+) {
+  console.error('Unexpected error occurred', err)
+  res.status(500).send({
+    message: 'Unexpected error occurred'
+  })
 }
